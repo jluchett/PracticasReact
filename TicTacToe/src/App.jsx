529d7aabff8d05e2c662c2dd8e6d0022bcc37fa6
jluchett <jluchett@hotmail.com>
2023-05-16 08:29:59 -0500

    Add modal y finish logic winner

--- TicTacToe/src/App.jsx
+++ TicTacToe/src/App.jsx
@@ -16,22 +16,77 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
     </div>
   );
 };
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    //iteramos las combos con un for each
+    for (const combo of WINNER_COMBOS) {
+      //destructuramos
+      const [a, b, c] = combo;
+      //comparamos el tablero con las combinaciones ganadoras
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] == boardToCheck[b] &&
+        boardToCheck[a] == boardToCheck[c]
+      ) {
+        //devolvemos el ganador
+        return boardToCheck[a];
+      }
+    }
+    //si no hay ganador devolvemos null
+    return null;
+  };
 
   const actualBoard = (index) => {
-    if (board[index]) return;
+    //si ya tiene algo en la celda no sobreescribe o si hay ganador se para el juego
+    if (board[index] || winner) return;
+    //actualizar tablero
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
-
+    //cambiar turno
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    //revisamos si hay ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }else if ( checkEndGame(newBoard)){
+      setWinner(false)
+    }
   };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const checkEndGame = (newBoard) => {
+    //Revisamos si hay empate accediendo en cada celda si esta es diferente de null
+    //lo hacemos con la funcion "every"
+    return newBoard.every((celda) => celda != null)
+  }
+
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset Game</button>
       <section className="game">
         {board.map((cel, index) => {
           return (
@@ -45,6 +100,20 @@ function App() {
         <Square isSelected={turn == TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn == TURNS.O}>{TURNS.O}</Square>
       </section>
+      {winner != null && (
+        <section className="winner">
+          <div className="text">
+            <h2>{winner == false ? "Empate" : "Ganó:"}</h2>
+
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+            <footer>
+              <button onClick={resetGame}>Empezar de nuevo</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
