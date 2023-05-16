e267819c9b5cdc20dd86f14e28860bcf546fc057
jluchett <jluchett@hotmail.com>
2023-05-16 09:51:20 -0500

    Add localStorage

--- TicTacToe/src/App.jsx
+++ TicTacToe/src/App.jsx
@@ -6,8 +6,15 @@ import { WinnerModal } from "./components/WinnerModal.jsx";
 
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState( () => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  });
   const [winner, setWinner] = useState(null);
 
   const checkWinner = (boardToCheck) => {
@@ -39,6 +46,9 @@ function App() {
     //cambiar turno
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    //guardamos la partida en el localstore
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
     //revisamos si hay ganador
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
@@ -53,6 +63,9 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   const checkEndGame = (newBoard) => {
